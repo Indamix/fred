836ef7c65ab9aaa0eb48160f896c3149ef71896b
Ivan <ivan.karasev@castoredc.com>
2020-01-07 23:03:07 +0100

    deploy

---
+++ precache-manifest.1daf275d1926517957344c401c429b17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b0b18b6ca7dfa4ae47d8bb3ec4a5ac",
+    "url": "/fred/index.html"
+  },
+  {
+    "revision": "ef6a0c21853179f0b6bc",
+    "url": "/fred/static/css/main.745a8d85.chunk.css"
+  },
+  {
+    "revision": "5e14b6a297ebb6387252",
+    "url": "/fred/static/js/2.a75547cf.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/fred/static/js/2.a75547cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef6a0c21853179f0b6bc",
+    "url": "/fred/static/js/main.495c48fb.chunk.js"
+  },
+  {
+    "revision": "0083740247af2b8e006b",
+    "url": "/fred/static/js/runtime-main.801213b8.js"
+  }
+]);
